0f13933d3cb7103df6ddfee5d2440433c5dbeb7d
remittor <remittor@gmail.com>
2020-10-27 16:48:29 +0300

    Cleanup utils.cpp and add SHLWAPI.LIB

--- src/utils.h
+++ src/utils.h
@@ -1,38 +1,77 @@
 #pragma once
 
-// Helper functions
-BOOL ConvertIsoDateToDateTime(char* pdatetimefield, FILETIME *ft);
-BOOL CreateIsoDateString(FILETIME *ft, char* buf); //yyyymmddhhmmss
-char* strlcpy(char* p, const char* p2, int maxlen);
-char* strlcat(char* p, const char* p2, int maxlen);
-WCHAR* wcslcpy2(WCHAR* p,const WCHAR* p2,int maxlen);
-WORD Crc16CalcBlock(char* p, int len, WORD crc16);
-LPTSTR strcatbackslash(LPTSTR thedir);
-LPTSTR strlcatforwardslash(LPTSTR thedir, int maxlen);
-char* strlcatbackslash(char* thedir, int maxlen);
-WCHAR* wcslcatbackslash(WCHAR* thedir, int maxlen);
-void cutlastbackslash(char* thedir);
-char* ReplaceBackslashBySlash(char* thedir);
-WCHAR* ReplaceBackslashBySlashW(WCHAR* thedir);
-char* ReplaceSlashByBackslash(char* thedir);
-WCHAR* ReplaceSlashByBackslashW(WCHAR* thedir);
-char* FormValidUrl(char* url, int maxlen, BOOL sendurlasunicode);
-BOOL UnixTimeToLocalTime(long* mtime, LPFILETIME ft);
-void MimeEncode(char* inputstr, char* outputstr, int maxlen);
-int MimeDecode(char* inputstr, int srclen, char* outputstr, int maxlen);
-void ReplaceEnvVars(char* buf, int buflen);
-void ReplaceSubString(char* buf, const char* fromstr, const char* tostr, int maxlen);
-BOOL ParseAddress(char* serverstring, char* addr,  unsigned short* port, int defport);
-BOOL IsNumericIPv6(char* addr);
-bool MultiFileMatchW(WCHAR* wild, WCHAR* name);
+#include <windows.h>
+#include <BaseTsd.h>
+#include <stdlib.h>
+#include <shlwapi.h>
+
+#ifndef _LPCBYTE_DEFINED
+#define _LPCBYTE_DEFINED
+typedef const BYTE *LPCBYTE;
+#endif
+#ifndef _LPCVOID_DEFINED
+#define _LPCVOID_DEFINED
+typedef const VOID *LPCVOID;
+#endif
 
 
 #ifndef countof
 #define countof(array) (sizeof(array)/sizeof(array[0]))
 #endif
 
-#define LoadStr(s, i) LoadString(hinst, (i), (s), countof(s)-1)
-
 #ifndef _itoa_s
-#define _itoa_s(nr,buf,sz,rdx) (_itoa(nr,buf,rdx))
+#define _itoa_s(nr,buf,sz,rdx)  _itoa((nr),(buf),(rdx))
 #endif
+
+/* String functions */
+
+#define LoadStr(s, i)  LoadString(hinst, (i), (s), countof(s)-1)
+
+LPSTR  strlcpy(LPSTR p, LPCSTR p2, size_t maxlen);
+LPSTR  strlcat(LPSTR p, LPCSTR p2, size_t maxlen);
+LPWSTR wcslcpy2(LPWSTR p, LPCWSTR p2, size_t maxlen);
+LPSTR  strcatbackslash(LPSTR thedir);
+LPSTR  strlcatforwardslash(LPSTR thedir, size_t maxlen);
+LPSTR  strlcatbackslash(LPSTR thedir, size_t maxlen);
+LPWSTR wcslcatbackslash(LPWSTR thedir, size_t maxlen);
+void   cutlastbackslash(LPSTR thedir);
+LPSTR  ReplaceBackslashBySlash(LPSTR thedir);
+LPWSTR ReplaceBackslashBySlashW(LPWSTR thedir);
+LPSTR  ReplaceSlashByBackslash(LPSTR thedir);
+LPWSTR ReplaceSlashByBackslashW(LPWSTR thedir);
+
+
+/* Time functions */
+
+__forceinline
+void SetInt64ToFileTime(FILETIME * ft, INT64 tm)
+{
+    INT64 * p = (INT64 *)ft;
+    *p = tm;
+}
+
+bool ConvSysTimeToFileTime(const LPSYSTEMTIME st, LPFILETIME ft);
+bool ConvertIsoDateToDateTime(LPCSTR pdatetimefield, LPFILETIME ft);
+bool CreateIsoDateString(LPFILETIME ft, LPSTR buf); //yyyymmddhhmmss
+bool UnixTimeToLocalTime(const time_t * mtime, LPFILETIME ft);
+
+
+/* BASE64 */
+
+int MimeEncodeData(LPCVOID indata, size_t inlen, LPSTR outstr, size_t maxlen);
+int MimeEncode(LPCSTR inputstr, LPSTR outputstr, size_t maxlen);
+int MimeDecode(LPCSTR inputstr, size_t inlen, LPVOID outdata, size_t maxlen);
+
+
+/* String Formating */
+
+void ReplaceEnvVars(LPSTR buf, size_t buflen);
+void ReplaceSubString(LPSTR buf, LPCSTR fromstr, LPCSTR tostr, size_t maxlen);
+bool ParseAddress(LPCSTR serverstring, LPSTR addr, WORD * port, int defport);
+bool IsNumericIPv6(LPCSTR addr);
+
+/* ======================== */
+
+bool MultiFileMatchW(LPCWSTR wild, LPWSTR name);
+
+
