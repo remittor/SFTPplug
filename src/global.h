b600b1a7a22da6787df6baecdc54e47c007e6294
remittor <remittor@gmail.com>
2020-11-17 17:36:02 +0300

    Enable debug log subsystem and other subsystems from "bst" lib

--- src/global.h
+++ src/global.h
@@ -35,8 +35,19 @@ typedef const VOID *LPCVOID;
 #define _itoa_s(nr,buf,sz,rdx)  _itoa((nr),(buf),(rdx))
 #endif
 
+#ifdef WFX_DEBUG
+#define BST_MAX_LOG_LEVEL   BST_LL_TRACE
+#else
+#define BST_MAX_LOG_LEVEL   BST_LL_WARNING
+#endif
+
 #ifdef __cplusplus
 #include "fsplugin.h"
+#include "bst/bst.hpp"
+#include "bst/log.hpp"
+#include "bst/string.hpp"
+#include "bst/list.hpp"
+#include "bst/scope_guard.hpp"
 #endif
 
 
